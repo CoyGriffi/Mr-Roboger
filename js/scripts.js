6c1b379afec3698a6ebe9e8d377b1d883fe265ef
coygriffi <coygriffi@gmail.com>
2021-11-19 15:10:21 -0800

    add if else staement for loop

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,16 @@ function robotOutput(number) {
   let counter = []
   for (let index = 0; index <= number; index +=1) {
     let roboTalk = index.toString();
-    
+    if (counter.includes(3)) {
+      return "Won't you be my neighbor?";
+    } else if (counter.includes(2)) {
+      return "Beep";
+    } else if (counter.includes(1)) {
+      return "Boop";
+    } else if (counter.includes(42)) {
+      return "ERROR ERROR ERROR PLEASE CONTACT ADMIN";
+    } else {
+      return counter
+    }
   }
 }
