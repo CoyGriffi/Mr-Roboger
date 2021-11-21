fbce5657582d7e0c84b7fd4bef37e0eef003c85b
coygriffi <coygriffi@gmail.com>
2021-11-21 10:49:59 -0800

    change methods to fix function issue bug still persists

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,32 @@
 //back-end logic
 function robotOutput(number) {
   let counter = []
-  for (let index = 0; index <= number; index +=1) {
-    let roboTalk = index.toString();
-    if (counter.includes(3)) {
-      return "Won't you be my neighbor?";
-    } else if (counter.includes(2)) {
+  for (let index = 0; index <= number; index ++) {
+    let roboTalk = counter.push(index);
+    if (roboTalk.includes(3)) {
+      counter.push ("Won't you be my neighbor?");
+    } else if (roboTalk.includes(2)) {
       return "Beep";
-    } else if (counter.includes(1)) {
+    } else if (roboTalk.includes(1)) {
       return "Boop";
-    } else if (counter.includes(42)) {
+    } else if (roboTalk.includes(42)) {
       return "ERROR ERROR ERROR PLEASE CONTACT ADMIN";
     } else {
       return counter
     }
   }
 }
+
+
+//UI logic
+
+$(document).ready(function() {
+  $("form#numberInput").submit(function(event) {
+    event.preventDefault();
+    let number = ($("input#number").val());
+    let results = robotOutput(number);
+      $("#outputText").show();
+      $("#outputNumber").text(results);
+
+  });
+});
